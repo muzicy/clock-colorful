35c93875f8b585676bdfd3374248e910abc68be5
licylk <1036814127@qq.com>
2016-12-04 20:47:33 +0800

    'clock'

---
+++ 倒计时时钟/countDown.js
@@ -0,0 +1,168 @@
+
+var WINDOW_WIDTH = document.documentElement.clientWidth;
+var WINDOW_HEIGHT = document.documentElement.clientHeight;
+	
+var RADIUS = WINDOW_WIDTH*0.8*(1/108)-1;
+var radius = WINDOW_WIDTH*0.8*(1/108)-1;
+var MARGIN_TOP = Math.floor(WINDOW_HEIGHT/5);
+var MARGIN_LEFT = Math.floor(WINDOW_WIDTH/10);
+	
+	
+var endTime = new Date();
+endTime.setTime(endTime.getTime()+2*3600000);
+
+var curShowTimeSeconds = 0
+var balls = [];
+
+window.onload = function(){
+    
+	
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT; 
+	curShowTimeSeconds = getCurrentShowTimeSeconds();   
+	
+	setInterval(function(){	   
+       render( context )
+	   update();
+	},50);
+}
+
+function update(){
+  	var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+	var nextHours = parseInt(nextShowTimeSeconds/3600);
+	var nextMinutes =  parseInt((nextShowTimeSeconds-nextHours*3600)/60);
+	var nextSeconds =parseInt( nextShowTimeSeconds%60);
+	
+	var curHours = parseInt(curShowTimeSeconds/3600);
+	var curMinutes =  parseInt((curShowTimeSeconds-curHours*3600)/60);
+	var curSeconds = parseInt(curShowTimeSeconds%60);
+	
+	if(curSeconds != nextSeconds) {
+		if(parseInt(curHours/10) != parseInt(nextHours/10)){
+		  addBalls(MARGIN_LEFT,MARGIN_TOP,parseInt(curHours/10));	
+		}
+		if(parseInt(curHours%10) != parseInt(nextHours%10)){
+		  addBalls(MARGIN_LEFT+15*(radius+1),MARGIN_TOP,parseInt(curHours%10));	
+		}
+		if(parseInt(curMinutes/10) != parseInt(nextMinutes/10)){
+		  addBalls(MARGIN_LEFT+39*(radius+1),MARGIN_TOP,parseInt(curHours/10));	
+		}
+		if(parseInt(curMinutes%10) != parseInt(nextMinutes%10)){
+		  addBalls(MARGIN_LEFT+54*(radius+1),MARGIN_TOP,parseInt(curMinutes%10));	
+		}
+		if(parseInt(curSeconds/10) != parseInt(nextSeconds/10)){
+		  addBalls(MARGIN_LEFT+78*(radius+1),MARGIN_TOP,parseInt(curSeconds/10));	
+		}
+		if(parseInt(curSeconds%10) != parseInt(nextSeconds%10)){
+		  addBalls(MARGIN_LEFT+93*(radius+1),MARGIN_TOP,parseInt(curSeconds%10));	
+		}
+		
+	  	curShowTimeSeconds = nextShowTimeSeconds;
+	}
+	
+	updateBalls();
+	//console.log(balls.length)
+	
+}
+
+function updateBalls(){
+ for (var i = 0; i < balls.length; i++){
+  balls[i].x += balls[i].vx;
+  balls[i].y += balls[i].vy;
+  balls[i].vy += balls[i].g;
+  
+  if (balls[i].y>=WINDOW_HEIGHT-radius){
+	balls[i].y = WINDOW_HEIGHT-radius;
+	balls[i].vy = -balls[i].vy*0.75;  
+  }  
+ }	
+ var cnt = 0;
+  for (var i = 0; i<balls.length; i++){
+	if((balls[i].x+radius)>0 && (balls[i].x-radius)<WINDOW_WIDTH){
+	  balls[cnt++]=balls[i];	
+	}  
+  } 
+  while(balls.length>Math.min(250,cnt)){
+	balls.pop();  
+  } 
+
+ 
+}
+
+function addBalls(x,y,num){
+	 for( var i = 0 ; i < digit[num].length ; i ++ ){
+        for(var j = 0 ; j < digit[num][i].length ; j ++ ) {
+            if( digit[num][i][j] == 1 ){
+				var aBall = {
+				  x:x+j*2*(RADIUS+1)+(RADIUS+1),
+				  y:y+i*2*(RADIUS+1)+(RADIUS+1),
+				  g:1.5+Math.random(),
+				  vx:Math.pow(-1,Math.ceil(Math.random()*1000))*4,
+				  vy:-5,
+				  color:randomColor(),
+				}
+				balls.push(aBall);
+			
+            }
+		}
+	}
+}
+
+function randomColor() {
+  var str =Math.round(Math.random()*(0xFFFFFF)).toString(16);
+  while(str.length < 6) str = "0" + str;
+  return "#" + str 
+ }
+
+function getCurrentShowTimeSeconds() {
+    var curTime = new Date();
+    var ret = endTime.getTime() - curTime.getTime();
+    ret = Math.round( ret/1000 )
+
+    return ret >= 0 ? ret : 0;
+}
+
+function render( cxt ){
+    cxt.clearRect(0,0,cxt.canvas.width,cxt.canvas.height);
+    var hours = parseInt( curShowTimeSeconds / 3600);
+    var minutes = parseInt( (curShowTimeSeconds - hours * 3600)/60 )
+    var seconds = parseInt(curShowTimeSeconds % 60)
+	
+    renderDigit( MARGIN_LEFT , MARGIN_TOP , parseInt(hours/10) , cxt )
+    renderDigit( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(hours%10) , cxt )
+    renderDigit( MARGIN_LEFT + 30*(RADIUS + 1) , MARGIN_TOP , 10 , cxt )
+    renderDigit( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(minutes/10) , cxt);
+    renderDigit( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(minutes%10) , cxt);
+    renderDigit( MARGIN_LEFT + 69*(RADIUS+1) , MARGIN_TOP , 10 , cxt);
+    renderDigit( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(seconds/10) , cxt);
+    renderDigit( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(seconds%10) , cxt);
+	for (var i = 0;i < balls.length; i++){
+	  cxt.fillStyle = balls[i].color;
+	 // alert(cxt.fillStyle );
+	  cxt.beginPath();
+	  cxt.arc(balls[i].x,balls[i].y,radius,0,2*Math.PI,true);
+	  cxt.closePath()
+	  cxt.fill();	
+	}
+}
+
+function renderDigit( x , y , num , cxt ){
+
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for( var i = 0 ; i < digit[num].length ; i ++ ){
+        for(var j = 0 ; j < digit[num][i].length ; j ++ ) {
+            if( digit[num][i][j] == 1 ){
+                cxt.beginPath();
+                cxt.arc( x+j*2*(RADIUS+1)+(RADIUS+1) , y+i*2*(RADIUS+1)+(RADIUS+1) , RADIUS , 0 , 2*Math.PI )
+                cxt.closePath()
+
+                cxt.fill()
+            }
+		}
+	}
+}
+
